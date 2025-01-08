3931d9b43d14ca1ced86e7a29daeb0444b6b015c
Scott Grisak <sgrisak@gmail.com>
2025-01-08 16:32:55 -0500

    Added a couple of sections and styles to play around

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!</Text>
+      <View>
+        <Text>Another piece of text!</Text>
+        </View>
+      <Text style={{margin: 16, borderWidth: 4, padding: 8, borderRadius: 10, borderColor: 'red'}}>Hello world!!</Text>
       <Button title="Push me!"/>
     </View>
   );
