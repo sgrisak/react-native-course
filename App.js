61f34c943f7dbf87bb76d3396b84f127cbee8318
Scott Grisak <sgrisak@gmail.com>
2025-01-12 14:39:46 -0500

    Added event handler and some styles

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+
+  function goalInputHandler(enteredText) {
+    // by doing something as simple as: console.log(enteredText), we could see the output 
+    // of each keystroke in the console of VSC. We will save the state of the input here
+    // so we can pass it to the next function, addGoalHandler
+    setEnteredGoalText(enteredText)
+  };
+
+  function addGoalHandler() {
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
+  };
+
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>Another piece of text!</Text>
-        </View>
-      <Text style={{margin: 16, borderWidth: 4, padding: 8, borderRadius: 10, borderColor: 'red'}}>Hello world!!</Text>
-      <Button title="Push me!"/>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        {/* onChangeText is a prop that will watch for text changes and then fire off the goalInputHandler 
+        function. It will then provide us with the enteredText and pass it to the function. */}
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}/>
+        <Button title='Add goal' onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 16,
+    flexDirection: 'column',
+    color: 'white',
+    backgroundColor: 'white',
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 4,
+  }
 });
